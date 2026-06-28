625a267889b1fb36d7315d43b09e2c7cee2c581a
finntastic <179311210+finntaztic@users.noreply.github.com>
2026-06-29 07:43:34 +0800

    'add gameboard file and ship constructor'

---
+++ gameboard.js
@@ -0,0 +1,14 @@
+class Ship {
+    constructor (size, hit, sunk){ //i added miss but it wouldnt be right, thats board
+        this.size = size;
+        this.hit = hit;
+        this.sunk = sunk;
+    }
+
+    receiveAttack (){
+        
+    }
+}
+
+
+
