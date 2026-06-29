c77ba8d814c77c8069c3629fa8fe9ceee48b74ca
finntastic <179311210+finntaztic@users.noreply.github.com>
2026-06-30 01:10:18 +0800

    create ship func, isSunk method, receiveAttack method

--- gameboard.js
+++ gameboard.js
@@ -1,14 +1,18 @@
-class Ship {
-    constructor (size, hit, sunk){ //i added miss but it wouldnt be right, thats board
-        this.size = size;
+export class Ship {
+    constructor (length, hit, isSunk){ //i added miss but it wouldnt be right, thats board
+        this.length = length;
         this.hit = hit;
-        this.sunk = sunk;
+        this.isSunk = isSunk;
     }
 
-    receiveAttack (){
-        
+    receiveAttack(){
+        return this.hit ++;
+    }
+
+    isSunk(){
+        if (this.length <= this.hit){
+            return this.isSunk = true;
+        } else return this.isSunk = false;
     }
 }
-
-
 
