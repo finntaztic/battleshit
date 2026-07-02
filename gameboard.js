ecd36da28d5d325c0e90fd378972fcd743cb0ea8
finntastic <179311210+finntaztic@users.noreply.github.com>
2026-07-02 11:20:14 +0800

    rename addhit() to hit ()

--- gameboard.js
+++ gameboard.js
@@ -1,12 +1,12 @@
 export class Ship {
     constructor (length){ 
         this.length = length;
-        this.hit = 0;
+        this.hitCount = 0;
         this.sunk = false;
     }
 
-    addHit(){
-        return ++this.hit;
+    hit(){
+        return ++this.hitCount;
     }
 
     isSunk(){
