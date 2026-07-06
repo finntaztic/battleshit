cabfdf5f4327394fb09f658ad3c95618c74c8c93
finntastic <179311210+finntaztic@users.noreply.github.com>
2026-07-07 04:48:44 +0800

    added placeship func & test

--- gameboard.js
+++ gameboard.js
@@ -10,14 +10,52 @@ export class Ship {
     }
 
     isSunk(){
-        if (this.length <= this.hit){
+        if (this.length <= this.hitCount){
             return this.sunk = true;
         } else return this.sunk = false;
     }
 }
 
 export class Gameboard{
-    addShip(){
-        
+    constructor(){
+        this.row = 10;
+        this.col = 10;
+        this.board = [];
     }
-}
+
+    createBoard(){
+        for (let i = 0; i < this.row; i++){
+            let rows = [];
+            for (let j = 0; j < this.col; j++){
+                rows.push(null)
+            }
+            this.board.push(rows)
+        }
+    }
+
+    placeShip(ship, x, y, orientation){
+        let board = this.board;
+        let addCell;
+        const length = ship.length;
+        const cells = []
+
+        for (let i = 0; i < length; i++){
+            if (orientation == 'horizontal'){
+                addCell = [x + i, y]
+                if (addCell[0] > 9 || addCell[0] < 0) return;
+                // else board[addCell[0]][addCell[1]] = ship;
+                console.log(addCell)
+            } else if (orientation == 'vertical'){
+                addCell = [x, y + i]
+                if (addCell[1] > 9 || addCell[1] < 0) return;
+                // else board[addCell[0]][addCell[1]] = ship;
+                console.log(addCell)
+            } 
+        }
+        return board[addCell[0]][addCell[1]]
+        // return addCell
+    }
+    }
+
+    //problem with this code is it still logs other cells. we wanna kill it immedietely
+    //perhaps adding the condition from the very start
--- test.js
+++ test.js
@@ -1,20 +1,46 @@
 import { Ship } from "./gameboard.js";
+import { Gameboard } from "./gameboard.js";
 
 let ship;
+let gameboard;
 
 beforeEach(() => {
     ship = new Ship();
-})
+    gameboard = new Gameboard();
+    gameboard.createBoard();
+});
 
 test('hit count', () => {
-    expect(ship.addHit()).toBe(1);
-})
+    expect(ship.hit()).toBe(1);
+});
 
 test('if sunk', () => {
     ship.length = 4;
-    ship.addHit();
-    ship.addHit();
-    ship.addHit();
-    ship.addHit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
     expect(ship.isSunk()).toBe(true)
-})
+});
+
+
+test ('if gameboard length is correct', () => {
+    expect(gameboard.board.length).toBe(10);    
+});
+
+test ('if ships being placed', () => {
+    ship = new Ship(5);
+    let board = gameboard.board;
+    gameboard.placeShip(ship, 2, 7, 'vertical')
+    expect(board[2][7]).toBeInstanceOf(Ship)
+});
+
+// test ('if ships not being placed when outside of grid', () => {
+//     ship = new Ship (5);
+//     let board = gameboard.board;
+//     // gameboard.placeShip(ship, 2, 7, 'horizontal')
+//     expect(console.log(gameboard.placeShip(ship, 2, 7, 'horizontal'))).toBe(
+//         [2,7]
+//     )
+    
+// })
